69635ff8ab5b20ba620fd0ec6ca009301d5d6364
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2022-08-05 15:48:04 +0300

    add test region

--- dashboard-react-app/src/HelloWorldItem.js
+++ dashboard-react-app/src/HelloWorldItem.js
@@ -1,5 +1,7 @@
+// #region imports
 import { CustomItemViewer, ResourceManager } from 'devexpress-dashboard/common'
 import { CustomItem } from 'devexpress-dashboard/model'
+// #endregion
 
 var svgIcon = '<svg id="helloWorldItemIcon" viewBox="0 0 24 24"><path stroke="#42f48f" fill="#42f48f" d="M12 2 L2 22 L22 22 Z" /></svg>';
 var helloWorldItemMetaData = {
